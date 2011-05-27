43076e76f0e104014a74c8111172574ec17b6d3a
Ganesh Kumar <ganesh@Ganeshs-iMac-5.local>
2011-05-27 17:45:38 +0530

    added settings menu and related code

--- src/org/example/hello/Sudoku.java
+++ src/org/example/hello/Sudoku.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.content.Intent;
 import android.view.View.OnClickListener;
 
+import android.view.Menu; 
+import android.view.MenuInflater; 
+import android.view.MenuItem;
+
 public class Sudoku extends Activity implements OnClickListener{
 	/** Called when the activity is first created. */
 	@Override
@@ -31,4 +35,22 @@ public class Sudoku extends Activity implements OnClickListener{
 			break;
 		}
 	}
+	
+	@Override 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu); 
+		MenuInflater inflater = getMenuInflater(); 
+		inflater.inflate(R.menu.menu, menu); 
+		return true;
+		}
+	
+	@Override 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) { case R.id.setting:
+		startActivity(new Intent(this, Prefs.class));
+		return true; // More items go here (if any) ... 
+		} 
+		return false;
+	}
 }
+
